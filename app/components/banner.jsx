8202a53c0e9a846c3bcfee3c5758c254ca1388c5
Johnson Fredrick <johnsonfredrick577@gmail.com>
2023-11-06 13:44:44 +0300

    changes

--- app/components/banner.jsx
+++ app/components/banner.jsx
@@ -5,12 +5,12 @@ export default function Banner() {
   return (
     <div className="relative h-screen">
       <Image src="/Banner.png" layout="fill" objectFit="cover" />
-      <div className="absolute bottom-0 right-0">
+      <div className="absolute bottom-0 right-0 w-1/2">
         <Image src="/truck.png" width={500} height={300} />
       </div>
-      <div className="absolute top-0 left-0 p-10">
-        <p>Your text here</p>
-        <p>More text here</p>
+      <div className="absolute top-0 left-0 p-10 w-1/2">
+        <p className="flex flex-wrap text-white-500 text-6xl">We are Transforming Logistics in Tanzania And Africa</p>
+        <p className="flex flex-wrap">Your trusted partner in cargo transportation, revolutionizing the logistics industry in Tanzania and across Africa</p>
       </div>
     </div>
   );
