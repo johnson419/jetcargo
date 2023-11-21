e8148af7b08d955f9ca54ae4dbe87f2698d008a3
johnson419 <johnsonfredrick577@gmail.com>
2023-11-21 15:04:55 +0300

    modified top header

--- app/components/topheader.jsx
+++ app/components/topheader.jsx
@@ -1,30 +1,39 @@
-import React from "react"
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 export default function topheader() {
-    return(
-<div className="bg-dark-blue">
-    
-       <div className="flex justify-start">
-       Follow us:
-       {/* font awesome icons here
-       facebook
-       twitter
-       linkedin
-       */}
-       </div>
-       <div className="flex justify-end ">
-        <div className="flex-nowrap">
-{/* call icon */}
-<p>Call: 1234 567 890</p>
-<p>Email: info@eac-yap.com</p>
-
+  return (
+    <div className="flex flex-row bg-dark-blue h-[43px]">
+      <div className="flex justify-start space-x-4">
+        Follow us:
+        <FontAwesomeIcon
+          icon={faFacebook}
+          style={{ width: "13px", height: "13px" }}
+        />
+        <FontAwesomeIcon
+          icon={faTwitter}
+          style={{ width: "13px", height: "13px" }}
+        />
+        <FontAwesomeIcon
+          icon={faLinkedin}
+          style={{ width: "13px", height: "13px" }}
+        />
+      </div>
+      <div className="flex flex-row ">
+        <div className="flex justify-end flex-row">
+            <FontAwesomeIcon
+                icon={faPhone}
+                style={{ width: "13px", height: "13px" }}/>
+          <p>Call: 1234 567 890</p>
+          <p>Email: info@eac-yap.com</p>
         </div>
-
-       </div>
-   
-
-
-</div>
-
-    )
+      </div>
+    </div>
+  );
 }
